0ddff589f5cbdc4bd7baa72d0b7b5c1a6914d81c
Ilya Kravtsov <krawzow1@yandex.ru>
2025-04-27 20:54:44 +0300

    Доб. категории в призме

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,3 +1,4 @@
+import { categories } from "./constants";
 import { prisma } from "./prisma-client"
 import {hashSync} from 'bcrypt'
 
@@ -20,6 +21,10 @@ async function up() {
                 role: 'ADMIN', 
             }
         ]
+    })
+
+    await prisma.category.createMany({
+        data: categories
     })
 
 }
